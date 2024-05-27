3514d2c388fd0d1fba712caca958c16931b804b8
Jack Adolph <jack@jaedolph.net>
2024-05-27 18:28:55 +1000

    Allow version to be included in zipfile artifacts

--- scripts/package_frontend.sh
+++ scripts/package_frontend.sh
@@ -1,6 +1,12 @@
 #!/bin/bash
 set -xe
 
+FILENAME=mulletwebhook-frontend.zip
+VERSION="$1"
+if [ -n ${VERSION} ]; then
+    FILENAME="mulletwebhook-frontend-${VERSION}.zip"
+fi
+
 # packages frontend deps for upload
 
 SCRIPTPATH="$( cd -- "$(dirname "$0")" >/dev/null 2>&1 ; pwd -P )"
@@ -8,5 +14,5 @@ SCRIPTPATH="$( cd -- "$(dirname "$0")" >/dev/null 2>&1 ; pwd -P )"
 mkdir -p ${SCRIPTPATH}/../dist
 
 cd ${SCRIPTPATH}/../frontend
-zip -r ${SCRIPTPATH}/../dist/mulletwebhook-frontend.zip .
+zip -r ${SCRIPTPATH}/../dist/${FILENAME} .
 cd -
